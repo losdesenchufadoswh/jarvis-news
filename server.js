eb852fa2b2e7862864b45ac64bde381099ab289f
Fernando <fernando@gmail.com>
2026-06-17 12:59:26 -0400

    Increase Express JSON payload limit for voice audio

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ---- Helpers de fecha -------------------------------------------------------
